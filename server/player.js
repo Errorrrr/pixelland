14a3c62833d8fafb8922ff7263c842d9d13921e4
Alexander <windy.peak.dev@gmail.com>
2019-01-07 14:57:46 +0300

    Комментарии

--- server/player.js
+++ server/player.js
@@ -1,16 +1,15 @@
 class Player{
-    /**
-    Характеристики персонажа
-    Переменная hp - очки здоровья персонажа
-    Переменная stan - время оглушения
-    Переменная lastPlayerHit - последний игрок, нанесший урон
+    /** @description Характеристики персонажа
+    * @param {int} hp указывает на количество здоровья у персонажа
+    * @param {int} stan указывает на время оглушения
+    * @param {string} lastPlayerHit указывает на последнего игрока, нанесшего урон персонажу
     */
     constructor(x, y){
         this.x = x;
         this.y = y;
         this.hp = 100;
         this.stan = 0;
-        this.lastPlayerHit = "";
+        this.lastPlayerHit = null;
         this.vector = 'down';
         this.ismove = false;
         this.sector_x = Math.floor((x) / 100);
@@ -20,36 +19,27 @@ class Player{
         this.attack_stage = -1;
         this.endOfAttack = 3;
     }
-    /**
-    Движение по оси y
+    /** @description Движение по оси y
     */
     vertical_move(y){
       this.y += y * this.speed;
     }
-    /**
-    Движение по оси x
+    /** @description Движение по оси x
     */
     horizontal_move(x){
         this.x += x * this.speed;
     }
-    /**
-    Метод атаки
+    /** @description Метод атаки
     */
-    attack() {
+    isAttack() {
 
     }
-    /**
-    Метод получения урона
+    /** @description Метод получения урона
     */
-    heat(player, pureDamage) {
+    isHeat(player, pureDamage) {
         player.hp -= pureDamage;
         player.stan = 2;
-    }
-    /**
-    Последний, кто нанёс урон
-    */
-    lastHitPlayer(player){
-        This.lastPlayerHit = player;
+        this.lastPlayerHit = player;
     }
 }
 
