728bf5b2e228bc6420f7029850149de5572e3354
unknown <genersite@gnail.com>
2019-01-24 22:58:53 +0300

    прiкол

--- server/player.js
+++ server/player.js
@@ -1,12 +1,12 @@
 class Player{
     /** @description Характеристики персонажа.
-    * @param {int} hp указывает на количество здоровья у персонажа.
-    * @param {int} stan указывает на время оглушения.
-    * @param {string} lastPlayerHit указывает на последнего игрока, нанесшего урон персонажу.
-    */
-    constructor(Socket, x, y, ActionHandler){
+     * @param {int} hp указывает на количество здоровья у персонажа.
+     * @param {int} stan указывает на время оглушения.
+     * @param {string} lastPlayerHit указывает на последнего игрока, нанесшего урон персонажу.
+     */
+    constructor(Socket, x, y, actionHandler){
         this.id = Socket;
-        this.ActionHandler = ActionHandler;
+        this.actionHandler = actionHandler;
         this.x = x;
         this.y = y;
         this.squadKit = [];
@@ -22,23 +22,34 @@ class Player{
         this.speed = 1;
         this.attack = false;
         this.attack_stage = -1;
-        this.endOfAttack = 12;
+        this.endOfAttack = 3;
+    }
+    init(tick){
+        var actionData = (this.actionHandler.initDo(tick));
+        this.horizontal_move(actionData['horizontalMove']);
+        this.vertical_move(actionData['verticalMove']);
+        this.ismove = actionData['isMove'];
+        this.vector = actionData['vector'];
+        console.log(actionData);
     }
     /** @description Движение по оси y.
-    */
+     */
     vertical_move(y){
         this.y += y * this.speed;
+        console.log(typeof y);
+        console.log(typeof this.speed);
+        console.log(typeof this.y);
     }
     /** @description Движение по оси x.
-    */
+     */
     horizontal_move(x){
         this.x += x * this.speed;
     }
     /** @description Метод атаки.
-    */
+     */
     doAttack(players) {
         var squadKit = this.getAttackSquad();
-        for (var id in players) { 
+        for (var id in players) {
             var player = players[id];
             if(id != this.id && player.isHeat(squadKit) == true){
                 player.heat(this, 50);
@@ -49,34 +60,34 @@ class Player{
         }
     }
     /** @description Метод получения урона.
-    * @param {number} player Сокет игрока, который нанес урон.
-    */
+     * @param {number} player Сокет игрока, который нанес урон.
+     */
     heat(player, pureDamage) {
         this.hp -= pureDamage;
         player.stan = 2;
         this.lastPlayerHit = player;
     }
     /** @description Попадает ли player?.
-    */
+     */
     isHeat(squadKit){
         if(this.x >= squadKit['x']-(squadKit['sideX']/2) && this.x <= squadKit['x']+(squadKit['sideX']/2) && this.y >= squadKit['y']-(squadKit['sideY']/2) && this.y <= squadKit['y']+(squadKit['sideY']/2)){
             return true;
         }
-        return false; 
+        return false;
     }
     /** @description Cмерть персонажа.
-    */
+     */
     death(playerKiller,countScore){
         playerKiller.addScore(countScore);
         //io.sockets.connected[player.id].disconnect(true);
     }
     /** @description Добавление очков.
-    */
+     */
     addScore(countScore,playerKiller){
         this.score += countScore;
     }
     /** @description Просчет координат и стороны прямоугольника атаки.
-    */
+     */
     getAttackSquad(){
         var x,y,sideX,sideY;
         if(this.vector == 'up'){
